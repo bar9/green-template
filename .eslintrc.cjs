f048a3be8465351bf74f9717350f524a12201f8b
rb <r.brand@iwf.ch>
2022-12-24 09:38:01 +0100

    add storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,28 +2,20 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:import/errors',
-    'plugin:import/typescript',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  overrides: [
-    {
-      files: ['**/*.ts'],
-      rules: {
-        'react/display-name': 0,
-      },
-    },
-  ],
+  extends: ['eslint:recommended', 'plugin:import/errors', 'plugin:import/typescript', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:storybook/recommended'],
+  overrides: [{
+    files: ['**/*.ts'],
+    rules: {
+      'react/display-name': 0
+    }
+  }],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
-    },
+      jsx: true
+    }
   },
   plugins: ['import', 'react', '@typescript-eslint', 'typescript-sort-keys'],
   rules: {
@@ -36,13 +28,9 @@ module.exports = {
     'array-bracket-spacing': [2, 'never'],
     'arrow-parens': [2, 'always'],
     'arrow-spacing': 2,
-    'brace-style': [
-      2,
-      '1tbs',
-      {
-        allowSingleLine: true,
-      },
-    ],
+    'brace-style': [2, '1tbs', {
+      allowSingleLine: true
+    }],
     'eol-last': 2,
     // `import/default` and `import/namespace` are slow.
     'import/default': 0,
@@ -70,19 +58,20 @@ module.exports = {
     semi: [2, 'always'],
     'sort-keys': 2,
     'space-before-blocks': 2,
-    'space-before-function-paren': [
-      2,
-      { anonymous: 'never', asyncArrow: 'always', named: 'never' },
-    ],
+    'space-before-function-paren': [2, {
+      anonymous: 'never',
+      asyncArrow: 'always',
+      named: 'never'
+    }],
     'typescript-sort-keys/interface': 2,
-    'typescript-sort-keys/string-enum': 2,
+    'typescript-sort-keys/string-enum': 2
   },
   settings: {
     'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
+      '@typescript-eslint/parser': ['.ts', '.tsx']
     },
     react: {
-      version: '18.0.0',
-    },
-  },
+      version: '18.0.0'
+    }
+  }
 };
